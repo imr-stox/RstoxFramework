0678d33f7bbb9e0179d6b1c90ba0e0aeb0581688
edvinf <edvin.fuglebakk@gmail.com>
2019-09-26 17:01:34 +0200

    added test suggestions for when issues in RStoxData gets fixed

--- tests/testthat/testxsd.R
+++ tests/testthat/testxsd.R
@@ -4,3 +4,8 @@ library(RstoxData)
 schema <- xml2::read_xml("../../inst/formats/stoxProject.xsd")
 expect_silent(processedschema <- RstoxData:::processXSD(schema))
 expect_true("project" %in% processedschema$rootInfo)
+xsdObjects$stoxv3.xsd <- RstoxData:::createXsdObject("../../inst/formats/stoxProject.xsd")
+
+# consider pending fix in RstoxData
+#context("read dummy xml")
+#readXmlFile("../../inst/testresources/dummy_project.xml", F, "stoxv3")

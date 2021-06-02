61bd22b3ba251ffa578561bead84c07402ac5ffc
Arne Johannes Holmin <arnejh@hi.no>
2021-06-03 01:34:13 +0200

    Test with printing 2 to file in getAbsolutePath()

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,6 +6,6 @@ write("newArgumentFiles", file.path(tempdir(), "heihei.txt"))
 # We have currently three test projects:
 options(Ncpus = 3L)
 
-test_check("RstoxFramework")
+try(test_check("RstoxFramework"))
 
 readLines(file.path(tempdir(), "heihei.txt"))
